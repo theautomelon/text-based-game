a374b899ff3262be9fb50797af20629bd0cc59c0
Austin Blackmon <jblack7@nmsu.edu>
2021-12-04 17:23:59 -0700

    closed #5 created monster.py
    Monster objects from monster.py have 5 datatypes:
    name - the monster's name
    species - the monster's species
    health - the monster's health
    attack - the monster's attack
    crit - the monster's crit-chance
    
    the health, attack, and crit-chance change depending on the monster's species

---
+++ monster.py
@@ -0,0 +1,37 @@
+debug = True
+
+
+class Monster:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+
+        if species == 'Badger':
+            self.health = 50
+            self.attack = 50
+            self.crit = 0
+        elif species == 'Bobcat':
+            self.health = 100
+            self.attack = 60
+            self.crit = 0
+        elif species == 'Coyote':
+            self.health = 150
+            self.attack = 70
+            self.crit = 0
+        elif species == 'Owl':
+            self.health = 100
+            self.attack = 100
+            self.crit = 0
+        elif species == 'Eagle':
+            self.health = 125
+            self.attack = 110
+            self.crit = 25
+
+        # print monster to when creating to validate
+        # this is purely for debugging
+        if debug:
+            print('Creating ' + str(self.name) + ' the ' + str(self.species) + ' with ' + str(self.health) + ' health, ' + str(self.attack) + ' attack, and ' + str(self.crit) + '%' + ' crit-chance')
+
+    
+
+Billy = Monster("Billy", "Badger")
