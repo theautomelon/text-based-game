12532ea4adb6c69eb27636cd0d8b7eea5d6edf9b
Austin Blackmon <jblack7@nmsu.edu>
2021-12-04 18:59:14 -0700

    closed #8 create combat system

--- monster.py
+++ monster.py
@@ -1,3 +1,6 @@
+import random as r
+from player import *
+
 debug = True
 
 
@@ -8,30 +11,43 @@ class Monster:
 
         if species == 'Badger':
             self.health = 50
-            self.attack = 50
+            self.attackDamage = 50
             self.crit = 0
         elif species == 'Bobcat':
             self.health = 100
-            self.attack = 60
+            self.attackDamage = 60
             self.crit = 5
         elif species == 'Coyote':
             self.health = 150
-            self.attack = 70
+            self.attackDamage = 70
             self.crit = 5
         elif species == 'Owl':
             self.health = 100
-            self.attack = 100
+            self.attackDamage = 100
             self.crit = 15
         elif species == 'Eagle':
             self.health = 125
-            self.attack = 110
+            self.attackDamage = 110
             self.crit = 25
 
         # print monster to when creating to validate
         # this is purely for debugging
         if debug:
-            print('Creating ' + str(self.name) + ' the ' + str(self.species) + ' with ' + str(self.health) + ' health, ' + str(self.attack) + ' attack, and ' + str(self.crit) + '%' + ' crit-chance')
+            print('Creating ' + str(self.name) + ' the ' + str(self.species) + ' with ' + str(self.health) + ' health, ' + str(self.attackDamage) + ' attack, and ' + str(self.crit) + '%' + ' crit-chance')
 
     def healthStatus(self):
-            print(str(self.name) + ' the ' + str(self.species) + ' has ' + str(self.health) + ' health')
+        print(str(self.name) + ' the ' + str(self.species) + ' has ' + str(self.health) + ' health')
     
+    def attack(self, player):
+        damage = r.randint(self.attackDamage -10,self.attackDamage +10)
+        
+        
+        if r.randint(0,100) < self.crit:
+            damage = damage * 2
+            print('It was a critical hit! ' + self.name + ' did ' + str(damage) + ' damage to you')
+        else:
+            print(self.name + ' did ' + str(damage) + ' damage to you')
+        
+        player.health = player.health - damage
+        if player.health <= 0:
+            print(str(self.name) + ' the ' + str(self.species) + ' killed you')
--- player.py
+++ player.py
@@ -1,3 +1,6 @@
+import random as r
+from monster import *
+
 debug = True
 
 class Player:
@@ -5,14 +8,32 @@ class Player:
         self.name = name
         self.species = 'Ferret'
         self.health = 100
-        self.attack = 50
+        self.attackDamage = 50
         self.crit = 10
 
         # print player to when creating to validate
         # this is purely for debugging
         if debug:
-            print('Creating ' + str(self.name) + ' the ' + str(self.species) + ' with ' + str(self.health) + ' health, ' + str(self.attack) + ' attack, and ' + str(self.crit) + '%' + ' crit-chance')
+            print('Creating ' + str(self.name) + ' the ' + str(self.species) + ' with ' + str(self.health) + ' health, ' + str(self.attackDamage) + ' attack, and ' + str(self.crit) + '%' + ' crit-chance')
 
     def healthStatus(self):
             print('You have ' + str(self.health) + ' health')
+            
+    def attack(self, monster):
+        damage = r.randint(self.attackDamage -10,self.attackDamage +10)
+        
+        
+        if r.randint(0,100) < self.crit:
+            damage = damage * 2
+            print('It was a critical hit! You did ' + str(damage) + ' damage to ' + monster.name + ' the ' + monster.species)
+        else:
+            print('You did ' + str(damage) + ' damage to ' + monster.name + ' the ' + monster.species)
+        
+        monster.health = monster.health - damage
+        if monster.health <= 0:
+            print('You killed ' + monster.name)
+        
+        
+
+
 
